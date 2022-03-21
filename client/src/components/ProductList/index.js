92a03c2ab37a0ccf07a7bd98a47848d4a1d88e05
tkcannon <taylorkcannon@gmail.com>
2022-03-21 11:54:38 -0600

    added pwa functionality

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -5,6 +5,7 @@ import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
@@ -20,9 +21,23 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
-    }
-  }, [data, dispatch]);
 
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+  
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
